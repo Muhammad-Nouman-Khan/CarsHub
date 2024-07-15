0f866b0d273a06dbb0aeccd25755d1dd5a1115d8
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2024-07-15 15:49:40 +0500

    cards

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 export async function fetchCars() {
-  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera";
 
   const headers = {
     "x-rapidapi-key": "c5c2cd5ae2msh9682779b9f6a5cbp119d66jsnc78aa2419e3e",
